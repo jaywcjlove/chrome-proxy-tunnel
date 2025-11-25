7bf2e5e7424b03ee546f8d33341c49dc9c839662
小弟调调 <398188662@qq.com>
2025-11-25 12:40:08 +0800

    init project.

--- install_deps.sh
+++ install_deps.sh
@@ -17,8 +17,18 @@ elif [ -f /etc/redhat-release ]; then
     # CentOS/RHEL
     echo "Detected CentOS/RHEL system."
     yum install -y alsa-lib.x86_64 atk.x86_64 cups-libs.x86_64 gtk3.x86_64 ipa-gothic-fonts libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXrandr.x86_64 libXScrnSaver.x86_64 libXtst.x86_64 pango.x86_64 xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-utils
+elif grep -q "Amazon Linux" /etc/os-release; then
+    # Amazon Linux 2023
+    echo "Detected Amazon Linux."
+    dnf install -y alsa-lib atk cups-libs gtk3 libXcomposite libXcursor libXdamage libXext libXi libXrandr libXScrnSaver libXtst pango xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-utils libdrm libgbm nss
+
 else
-    echo "Unsupported Linux distribution. Please install dependencies manually."
+    echo "Unsupported Linux distribution."
+    if [ -f /etc/os-release ]; then
+        echo "OS Release Info:"
+        cat /etc/os-release
+    fi
+    echo "Please install dependencies manually."
     echo "See: https://pptr.dev/troubleshooting#chrome-doesnt-launch-on-linux"
 fi
 
